d6fdf0f38657c0a94db56037b240c97d16d30af2
djoplo1 <djoplo1@wgu.edu>
2024-10-22 16:01:56 -0500

    added more combos to counter_list

--- DrillGenerator.py
+++ DrillGenerator.py
@@ -9,7 +9,10 @@ import random
 counter_list: list[str] = []
 counter_list.append("Parry Teep - Left Liver Shot - Left Hook - Low Kick.")
 counter_list.append("LeanBack from Hook - Rear Body Kick - Cross - Lead Upper Elbow.")
-
+counter_list.append("Defend Lead Hand Parry - Left Hook - Low Kick.")
+counter_list.append("Parry Cross - Lead Downward Elbow - Knee.")
+counter_list.append("Slip Jab - Cross - Lead Upper - Low Kick.")
+counter_list.append("Cross Check Inside Leg - Low Kick - Teep.")
 #COUNTER ENDS
 
 #DRILL DATABASE ENDS HERE
@@ -18,7 +21,7 @@ counter_list.append("LeanBack from Hook - Rear Body Kick - Cross - Lead Upper El
 
 
 #FUNCTION STARTS HERE
-print("Remember to do each drill with the intent of flow and nothingness.")
+print("Remember to do each drill with the intent of flow and nothingness. Add Exits for safety.")
 print(" Counters\n Weaknesses \n Blitzing Combos \n Initiation")
 user_input: str = input("What do you wish to work on? ")
 
